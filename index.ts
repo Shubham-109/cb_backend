c4e1713a694fe42421a0a6df2146501a240627a9
Shubham <shubhammeher109@gmail.com>
2023-10-30 00:06:38 +0530

    initiating...

--- index.ts
+++ index.ts
@@ -7,7 +7,11 @@ app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: true }));
 
 app.get("/", (req: Request, res: Response) => {
-  res.send("Healthy");
+  const ssid = req.query.SSID as string;
+  const password = req.query.pass as string;
+
+  if (!ssid || !password) return res.send("Invalid Params............!");
+  res.json({ ssid, password });
 });
 
 const PORT = process.env.PORT || 8000;
